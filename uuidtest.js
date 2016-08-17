b71a6015e3441143de3536d6046f3e7fa03ca64a
Tom Fagerland <tom@fagerland.org>
2016-08-17 14:53:30 +0200

    Starting to make progress on benchmark feature.

--- uuidtest.js
+++ uuidtest.js
@@ -1,15 +1,41 @@
 const uuid = require('node-uuid');
 const colors = require('colors');
-const pretty = require('pretty-cli');
-let array = [];
-let matched = false;
-let currentTime = new Date();
-while (!matched) {
-  let current = uuid.v4();
-  if (array.length % 1000 === 0) {
-    time();
+const commander = require('commander');
+const pretty = require('pretty-cli')();
+
+commander.version('0.2')
+  .option('-n, --number', 'Specify the number of UUIDs to test')
+  .option('-z, --forever', 'Run until aborted')
+  .option('-b, --benchmark', 'Test 100.000 UUIDs and see how long it takes')
+  .option('-t, --test', 'Generate UUIDs and test against initial UUID to see if one of them match those already created')
+  .parse(process.argv);
+
+let currentTime;
+start(commander);
+
+function start(args){
+  currentTime = new Date();
+}
+
+function test (n = -1 , cb) {
+  let array = [];
+  let matched = false;
+  if (array.length === n) {
+    return cb();
   }
-  match(current);
+  while (!matched) {
+    let current = uuid.v4();
+    if (array.length % 1000 === 0) {
+      time();
+    }
+    return match(current);
+  }
+}
+function benchmark(n = -1, function(){
+
+}){
+  let iterations = n>0? n: 100000;
+  test()
 }
 function match (current) {
   if (array.includes(current)) {
@@ -24,7 +50,7 @@ function time (cb, current) {
   let result = now - currentTime;
   currentTime = now;
   let message = `Now comparing against ${array.length} UUIDs. The last 1000 comparisons took ${msToTime(result)}`;
-  console.info(message.yellow);
+  pretty.info(message.yellow);
 }
 function msToTime (s) {
   var ms = s % 1000;
